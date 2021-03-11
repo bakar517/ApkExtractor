1678368f68149d48674684cf84f40282d5727040
Abu Bakar Hussain <abubakarhussain517@gmail.com>
2021-03-12 00:06:40 +0500

    - refactor.

---
+++ app/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+    id("kotlin-kapt")
+}
+
+android {
+    compileSdkVersion(AppConfig.compileSdk)
+    buildToolsVersion(AppConfig.buildToolsVersion)
+
+    defaultConfig {
+        applicationId = AppConfig.application_id
+        minSdkVersion(AppConfig.minSdk)
+        targetSdkVersion(AppConfig.targetSdk)
+        versionCode = AppConfig.versionCode
+        versionName = AppConfig.versionName
+
+        testInstrumentationRunner = AppConfig.androidTestInstrumentation
+    }
+
+    viewBinding {
+        android.buildFeatures.viewBinding = true
+        android.buildFeatures.dataBinding = true
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        getByName("debug") {
+            isMinifyEnabled = false
+        }
+    }
+
+    flavorDimensions(AppConfig.dimension)
+    productFlavors {
+        create("staging") {
+            applicationIdSuffix = ".staging"
+            dimension(AppConfig.dimension)
+        }
+
+        create("production") {
+            dimension(AppConfig.dimension)
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+}
+
+dependencies {
+    //std lib
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    //app libs
+    implementation(AppDependencies.appLibraries)
+    kapt(AppDependencies.kapt)
+    //test libs
+    testImplementation(AppDependencies.testLibraries)
+    androidTestImplementation(AppDependencies.androidTestLibraries)
+    debugImplementation(AppDependencies.debugImplementation)
+
+}
