7d48ef0563b7b56c219cbf05d45f4e299b2261dd
Abu Bakar Hussain <abubakarhussain517@gmail.com>
2021-03-13 17:57:41 +0500

    - refactor adapter class.

--- app/src/main/java/com/warlox/apkextractor/ui/appList/adapter/ApplicationListAdapter.kt
+++ app/src/main/java/com/warlox/apkextractor/ui/appList/adapter/ApplicationListAdapter.kt
@@ -3,57 +3,27 @@ package com.warlox.apkextractor.ui.appList.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.ListAdapter
 import com.warlox.apkextractor.R
 import com.warlox.apkextractor.data.model.ApplicationModel
 import com.warlox.apkextractor.databinding.ViewListApplicationBinding
 import com.warlox.apkextractor.ui.appList.ApplicationRecycleViewItemClick
-import com.warlox.apkextractor.util.ApplicationListDiffUtil
+import com.warlox.apkextractor.ui.appList.view_holder.ViewHolder
+import com.warlox.apkextractor.util.ApplicationModelDiffUtil
 
-class ApplicationListAdapter(val itemClickListener: ApplicationRecycleViewItemClick): RecyclerView.Adapter<ApplicationListAdapter.ViewHolder>() {
-
-    private val list = mutableListOf<ApplicationModel>()
-
-    fun setApplicationList(list:List<ApplicationModel>){
-        val result = DiffUtil.calculateDiff(ApplicationListDiffUtil(this.list, list))
-        this.list.clear()
-        this.list.addAll(list)
-        result.dispatchUpdatesTo(this)
-    }
+class ApplicationListAdapter(private val listener: ApplicationRecycleViewItemClick) :
+        ListAdapter<ApplicationModel, ViewHolder>(ApplicationModelDiffUtil) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
 
-        val binding = DataBindingUtil.inflate(layoutInflater, R.layout.view_list_application, parent,false) as ViewListApplicationBinding
+        val binding = DataBindingUtil.inflate(layoutInflater, R.layout.view_list_application, parent, false) as ViewListApplicationBinding
 
-        return ViewHolder(binding)
-
-//        val view = layoutInflater.inflate(R.layout.view_list_application, parent,false)
-//        return ViewHolder(view)
+        return ViewHolder(binding, listener)
     }
-
-    override fun getItemCount(): Int = list.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bindData(list[position],itemClickListener)
+        holder.bind(getItem(position))
     }
-
-
-    inner class ViewHolder(private val _itemView: ViewListApplicationBinding) :RecyclerView.ViewHolder(_itemView.root){
-        fun bindData(model: ApplicationModel, itemClickListener: ApplicationRecycleViewItemClick) {
-            _itemView.applicationModel = model
-            _itemView.executePendingBindings()
-            _itemView.root.setOnClickListener { itemClickListener.onApplicationListItemClick(model) }
-        }
-    }
-
-
-//    inner class ViewHolder(itemView: View) :RecyclerView.ViewHolder(itemView){
-//        fun bindData(model: ApplicationModel){
-//            itemView.appNameView.text = model.appName
-//            itemView.appIconView.setImageDrawable(model.icon)
-//        }
-//    }
 
 }
